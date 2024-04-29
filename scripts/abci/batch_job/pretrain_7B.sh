a7f24e4f07d98202398ba92c3e473d77be9f9b96
5ei74R0 <otoxukk.y@gmail.com>
2024-04-29 18:37:32 +0900

    :ambulance: Use `--env` and avoid the use of `$1`

--- scripts/abci/batch_job/pretrain_7B.sh
+++ scripts/abci/batch_job/pretrain_7B.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#$ -l rt_AF=1 (A100x8)
-#$ -l h_rt=5:00:00 (3.5h/A100x8に1.5hのバッファ)
+#$ -l rt_AF=1
+#$ -l h_rt=5:00:00
 #$ -j y
 #$ -l USE_SSH=1
 #$ -v SSH_PORT=2299
 #$ -cwd
 
 TRANSFORMERS_CACHE=$(pwd)/hub
-WANDB_KEY=$1
+WANDB_KEY=<insert-api-key>  # Insert your Weights & Biases API key here ...
 
 source /etc/profile.d/modules.sh
 module load singularitypro
-singularity run --compat --nv -B $(pwd):/src -B $TRANSFORMERS_CACHE:/hub llava.sif WANDB_API_KEY=$WANDB_KEY sh scripts/v1_5/pretrain_7B.sh
+singularity run --compat --nv -B $(pwd):/src -B $TRANSFORMERS_CACHE:/hub --env WANDB_API_KEY=$WANDB_KEY llava.sif sh scripts/v1_5/pretrain_7B.sh
