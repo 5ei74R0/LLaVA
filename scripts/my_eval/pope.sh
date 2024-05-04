0b23b2ae63b5ee40290b8733a678304c021148c6
5ei74R0 <otoxukk.y@gmail.com>
2024-05-04 16:09:13 +0900

    :adhesive_bandage: Set answer file path dynamically

--- scripts/my_eval/pope.sh
+++ scripts/my_eval/pope.sh
@@ -2,6 +2,7 @@
 
 MODEL_PATH=${1:-"./checkpoints/llava-v1.5-7b-lora"}
 MODEL_BASE=${2:-$(python3 -c "import json; print(json.load(open('$MODEL_PATH/config.json'))['_name_or_path'])")}
+CKPT=$(basename $MODEL_PATH)
 
 python -m llava.eval.model_vqa_loader \
     --model-path $MODEL_PATH \
@@ -9,11 +10,11 @@ python -m llava.eval.model_vqa_loader \
     --use-flash-attn \
     --question-file ./playground/data/eval/pope/llava_pope_test.jsonl \
     --image-folder ./playground/data/eval/pope/val2014 \
-    --answers-file ./playground/data/eval/pope/answers/llava-v1.5-7b-lora.jsonl \
+    --answers-file ./playground/data/eval/pope/answers/$CKPT.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
 
 python llava/eval/eval_pope.py \
     --annotation-dir ./playground/data/eval/pope/coco \
     --question-file ./playground/data/eval/pope/llava_pope_test.jsonl \
-    --result-file ./playground/data/eval/pope/answers/llava-v1.5-7b-lora.jsonl
+    --result-file ./playground/data/eval/pope/answers/$CKPT.jsonl
