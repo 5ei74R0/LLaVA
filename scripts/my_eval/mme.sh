d0a66803d0bee4bd4b6612b60777a24124ac2cbc
5ei74R0 <otoxukk.y@gmail.com>
2024-05-05 13:24:58 +0900

    :sparkles: Add eval script for mme

---
+++ scripts/my_eval/mme.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MODEL_PATH=${1:-"./checkpoints/llava-v1.5-7b-lora"}
+MODEL_BASE=${2:-$(python3 -c "import json; print(json.load(open('$MODEL_PATH/config.json'))['_name_or_path'])")}
+CKPT=$(basename $MODEL_PATH)
+
+python -m llava.eval.model_vqa_loader \
+    --model-path $MODEL_PATH \
+    --model-base $MODEL_BASE \
+    --use-flash-attn \
+    --question-file ./playground/data/eval/MME/llava_mme.jsonl \
+    --image-folder ./playground/data/eval/MME/MME_Benchmark_release_version \
+    --answers-file ./playground/data/eval/MME/answers/$CKPT.jsonl \
+    --temperature 0 \
+    --conv-mode vicuna_v1
+
+cd ./playground/data/eval/MME
+
+python convert_answer_to_mme.py --experiment $CKPT
+
+cd eval_tool
+
+python calculation.py --results_dir answers/$CKPT
