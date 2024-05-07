3df2c146d32425ca8003a0a6500a008d60b79858
5ei74R0 <otoxukk.y@gmail.com>
2024-05-07 18:34:53 +0900

    :sparkles: Add training scripts for llava-phi3

---
+++ scripts/abci/batch_job/finetune_phi3_lora.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -l rt_AF=1
+#$ -l h_rt=11:00:00
+#$ -j y
+#$ -l USE_SSH=1
+#$ -v SSH_PORT=2299
+#$ -cwd
+
+HF_HUB_CACHE=$(pwd)/hub
+WANDB_KEY=<insert-api-key>  # Insert your Weights & Biases API key here ...
+
+source /etc/profile.d/modules.sh
+module load singularitypro
+singularity run --compat --nv -B $(pwd):/src -B $HF_HUB_CACHE:/hub --env WANDB_API_KEY=$WANDB_KEY llava.sif sh scripts/v1_5/finetune_phi3_lora.sh
